7e2c3a21e10a08412a7a43036f960c009f0d468f
Marchal Tommy <marchal.tommy@gmail.com>
2024-10-29 23:58:29 +0100

    Version Bump to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.mtdevelopment.lafromagerie"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.2"
+        versionCode = 9
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
