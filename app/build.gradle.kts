0a53d14792815a73db8eaf05d06af2890ea1ea9e
Marchal Tommy <marchal.tommy@gmail.com>
2024-11-22 19:24:39 +0100

    Version Bump to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.mtdevelopment.lafromagerie"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
