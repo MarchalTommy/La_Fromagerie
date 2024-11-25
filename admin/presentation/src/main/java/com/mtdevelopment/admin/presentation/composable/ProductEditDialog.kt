09172150c1e1be5aafb456f5697f9065582c8f1b
Tommy MARCHAL <70965412+MarchalTommy@users.noreply.github.com>
2024-11-25 12:27:53 +0100

    * Fixed bad focus management caused by Dialog usage. Replaced the real "Dialog" composable with a surface with a semi-transparent background, and a manual handling of back press

--- admin/presentation/src/main/java/com/mtdevelopment/admin/presentation/composable/ProductEditDialog.kt
+++ admin/presentation/src/main/java/com/mtdevelopment/admin/presentation/composable/ProductEditDialog.kt
@@ -1,19 +1,27 @@
 package com.mtdevelopment.admin.presentation.composable
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -25,12 +33,12 @@ import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.mtdevelopment.core.presentation.sharedModels.UiProductObject
+import com.mtdevelopment.core.presentation.theme.ui.black70
 import com.mtdevelopment.core.util.toLongPrice
 import com.mtdevelopment.core.util.toUiPrice
 
@@ -42,6 +50,17 @@ fun ProductEditDialog(
     onError: (String) -> Unit,
     product: UiProductObject
 ) {
+    val focusRequester = remember {
+        FocusRequester()
+    }
+    val focusManager = LocalFocusManager.current
+
+    val scrollState = rememberScrollState()
+
+    val deleteFirstClick = remember {
+        mutableStateOf(false)
+    }
+
     val tempProduct = remember {
         mutableStateOf(
             UiProductObject(
@@ -56,140 +75,154 @@ fun ProductEditDialog(
         )
     }
 
-    // TODO: Fix focus
-    // TODO: Fix scroll
-    val focusRequester = remember {
-        FocusRequester()
-    }
-    val focusManager = LocalFocusManager.current
-
-    val scrollState = rememberScrollState()
-
-    val deleteFirstClick = remember {
-        mutableStateOf(false)
+    BackHandler(true) {
+        onDismiss.invoke()
     }
 
-    Dialog(
-        onDismissRequest = onDismiss,
-        properties = DialogProperties(
-            dismissOnBackPress = false,
-            dismissOnClickOutside = false
-        )
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = black70
     ) {
         Card(
             modifier = Modifier
-                .padding(16.dp)
-                .scrollable(scrollState, orientation = Orientation.Vertical)
                 .fillMaxWidth()
-                .wrapContentHeight(align = Alignment.CenterVertically)
+                .wrapContentHeight()
+                .padding(48.dp)
         ) {
-            ProductEditField(
-                title = "Nom du produit",
-                value = tempProduct.value.name,
-                onValueChange = {
-                    tempProduct.value = tempProduct.value.copy(name = it)
-                },
-                isError = tempProduct.value.name.isEmpty(),
-                imeAction = ImeAction.Next,
-                focusRequester = focusRequester,
-                focusManager = focusManager,
-            )
-            ProductEditField(
-                title = "Prix",
-                value = if (tempProduct.value.priceInCents != 0L) {
-                    tempProduct.value.priceInCents.toUiPrice().replace("€", "")
-                } else {
-                    ""
-                },
-                onValueChange = {
-                    try {
-                        if (it.toLongPrice() < 10000) {
-                            tempProduct.value =
-                                tempProduct.value.copy(priceInCents = it.toLongPrice())
-                        }
-                    } catch (e: NumberFormatException) {
-                        focusManager.clearFocus()
-                        onError.invoke("Vous ne pouvez pas mettre plusieurs virgules / points")
-                    }
-
-                },
-                isError = tempProduct.value.priceInCents == 0L,
-                isNumberOnly = true,
-                imeAction = ImeAction.Next,
-                focusRequester = focusRequester,
-                focusManager = focusManager,
-            )
-            ProductEditField(
-                title = "Description",
-                value = tempProduct.value.description,
-                onValueChange = {
-                    tempProduct.value = tempProduct.value.copy(description = it)
-                },
-                isError = tempProduct.value.description.isEmpty(),
-                isBigText = true,
-                imeAction = ImeAction.Next,
-                focusRequester = focusRequester,
-                focusManager = focusManager,
-            )
-            ProductEditField(
-                title = "Allergènes",
-                value = tempProduct.value.allergens?.joinToString { it } ?: "",
-                onValueChange = {
-                    tempProduct.value = tempProduct.value.copy(allergens =
-                    it.split(",").map { allergen ->
-                        allergen.trim().replaceFirstChar { firstChar -> firstChar.uppercaseChar() }
-                    }
+            Column(
+                modifier = Modifier
+                    .scrollable(
+                        state = scrollState,
+                        orientation = Orientation.Vertical
                     )
-                },
-                imeAction = ImeAction.Done,
-                focusRequester = focusRequester,
-                focusManager = focusManager,
-            )
-
-            Row(
-                modifier = Modifier,
-                horizontalArrangement = Arrangement.Start
+                    .wrapContentHeight(align = Alignment.CenterVertically)
+                    .focusable(true)
             ) {
-                Text(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .clickable {
-                            if (!deleteFirstClick.value) {
-                                deleteFirstClick.value = true
-                            } else {
-                                onDelete.invoke(product)
-                                onDismiss.invoke()
-                            }
-                        },
-                    text = if (!deleteFirstClick.value) {
-                        "SUPPRIMER"
-                    } else {
-                        "CONFIRMER ?"
+                ProductEditField(
+                    title = "Nom du produit",
+                    value = tempProduct.value.name,
+                    onValueChange = {
+                        tempProduct.value = tempProduct.value.copy(name = it)
                     },
-                    color = MaterialTheme.colorScheme.error
+                    isError = tempProduct.value.name.isEmpty(),
+                    imeAction = ImeAction.Next,
+                    focusRequester = focusRequester,
+                    focusManager = focusManager,
+                )
+                ProductEditField(
+                    title = "Prix",
+                    value = if (tempProduct.value.priceInCents != 0L) {
+                        tempProduct.value.priceInCents.toUiPrice().replace("€", "")
+                    } else {
+                        ""
+                    },
+                    onValueChange = {
+                        try {
+                            if (it.toLongPrice() < 10000) {
+                                tempProduct.value =
+                                    tempProduct.value.copy(priceInCents = it.toLongPrice())
+                            }
+                        } catch (e: NumberFormatException) {
+                            focusManager.clearFocus()
+                            onError.invoke("Vous ne pouvez pas mettre plusieurs virgules / points")
+                        }
+
+                    },
+                    isError = tempProduct.value.priceInCents == 0L,
+                    isNumberOnly = true,
+                    imeAction = ImeAction.Next,
+                    focusRequester = focusRequester,
+                    focusManager = focusManager,
+                )
+                ProductEditField(
+                    title = "Description",
+                    value = tempProduct.value.description,
+                    onValueChange = {
+                        tempProduct.value = tempProduct.value.copy(description = it)
+                    },
+                    isError = tempProduct.value.description.isEmpty(),
+                    isBigText = true,
+                    imeAction = ImeAction.Next,
+                    focusRequester = focusRequester,
+                    focusManager = focusManager,
+                )
+                ProductEditField(
+                    title = "Allergènes",
+                    value = tempProduct.value.allergens?.joinToString { it } ?: "",
+                    onValueChange = {
+                        tempProduct.value = tempProduct.value.copy(allergens =
+                        it.split(",").map { allergen ->
+                            allergen.trim()
+                                .replaceFirstChar { firstChar -> firstChar.uppercaseChar() }
+                        }
+                        )
+                    },
+                    imeAction = ImeAction.Done,
+                    focusRequester = focusRequester,
+                    focusManager = focusManager,
                 )
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Start
                 ) {
-                    Text("Valider",
-                        Modifier
-                            .padding(16.dp)
+                    Row(
+                        modifier = Modifier
                             .clickable {
-                                if (tempProduct.value != product) {
-                                    onValidate.invoke(tempProduct.value)
+                                if (!deleteFirstClick.value) {
+                                    deleteFirstClick.value = true
+                                } else {
+                                    onDelete.invoke(product)
+                                    onDismiss.invoke()
                                 }
-                                onDismiss.invoke()
-                            })
-                    Text("Annuler",
-                        Modifier
-                            .padding(16.dp)
-                            .clickable {
-                                onDismiss.invoke()
-                            })
+                            },
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            modifier = Modifier.padding(start = 8.dp),
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete Product",
+                            tint = MaterialTheme.colorScheme.error
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 16.dp, end = 8.dp, bottom = 16.dp),
+                            text = if (!deleteFirstClick.value) {
+                                "SUPPRIMER"
+                            } else {
+                                "CONFIRMER ?"
+                            },
+                            maxLines = 2,
+                            softWrap = false,
+                            fontWeight = FontWeight.Black,
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Text("Valider",
+                            Modifier
+                                .clickable {
+                                    if (tempProduct.value != product) {
+                                        onValidate.invoke(tempProduct.value)
+                                    }
+                                    onDismiss.invoke()
+                                }
+                                .padding(vertical = 16.dp, horizontal = 8.dp)
+                        )
+                        Text("Annuler",
+                            Modifier
+                                .clickable {
+                                    onDismiss.invoke()
+                                }
+                                .padding(vertical = 16.dp, horizontal = 8.dp)
+                        )
+                    }
                 }
             }
+
 
         }
     }
@@ -210,6 +243,7 @@ fun ProductEditField(
     OutlinedTextField(
         modifier = Modifier
             .padding(8.dp)
+            .fillMaxWidth()
             .focusRequester(focusRequester),
         value = value,
         onValueChange = {
@@ -227,15 +261,19 @@ fun ProductEditField(
         maxLines = if (isBigText) Int.MAX_VALUE else 1,
         isError = isError,
         keyboardOptions = KeyboardOptions(
-            keyboardType = if (isNumberOnly) KeyboardType.Number else KeyboardType.Text,
-            imeAction = imeAction
+            imeAction = imeAction,
+            keyboardType = if (isNumberOnly) {
+                KeyboardType.Number
+            } else {
+                KeyboardType.Text
+            },
         ),
         keyboardActions = KeyboardActions(
             onNext = {
                 focusManager.moveFocus(FocusDirection.Down)
             },
             onDone = {
-                focusManager.clearFocus()
+                focusManager.clearFocus(force = true)
             }
         )
     )
