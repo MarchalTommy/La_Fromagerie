d955e320ad01f0a9b21718ddae284ac9855cd2fc
Tommy MARCHAL <70965412+MarchalTommy@users.noreply.github.com>
2024-11-29 10:39:42 +0100

    Repaired refresh on homeScreen

--- home/presentation/src/main/java/com/mtdevelopment/home/presentation/viewmodel/HomeViewModel.kt
+++ home/presentation/src/main/java/com/mtdevelopment/home/presentation/viewmodel/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.mtdevelopment.home.presentation.viewmodel
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -13,9 +11,6 @@ import com.mtdevelopment.home.domain.usecase.GetAllCheesesUseCase
 import com.mtdevelopment.home.domain.usecase.GetAllProductsUseCase
 import com.mtdevelopment.home.domain.usecase.GetLastFirestoreDatabaseUpdateUseCase
 import com.mtdevelopment.home.presentation.state.HomeUiState
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
@@ -31,37 +26,29 @@ class HomeViewModel(
     var homeUiState by mutableStateOf(HomeUiState())
         private set
 
-    private val _isRefreshReady: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    private val isRefreshReady: StateFlow<Boolean> = _isRefreshReady.asStateFlow()
-
     init {
         viewModelScope.launch {
-            getLastFirestoreDatabaseUpdateUseCase.invoke(onSuccess = {
-                this.launch {
-                    getAllProducts()
-                }
-            }, onFailure = {
-                homeUiState = homeUiState.copy(
-                    isLoading = false,
-                    isError = "Mise à jour de la base de donnée impossible"
-                )
-            })
+            checkAndUpdateDatabase()
         }
     }
 
-    private suspend fun getAllProducts() {
-        Log.i(
-            TAG,
-            "NOW GETTING ALL PRODUCTS"
-        )
+    private suspend fun checkAndUpdateDatabase() {
+        getLastFirestoreDatabaseUpdateUseCase.invoke(onSuccess = {
+            viewModelScope.launch {
+                getAllProducts()
+            }
+        }, onFailure = {
+            homeUiState = homeUiState.copy(
+                isLoading = false,
+                isError = "Mise à jour de la base de donnée impossible"
+            )
+        })
+    }
 
+    private suspend fun getAllProducts() {
         getAllProductsUseCase.invoke(
             scope = viewModelScope,
             onSuccess = { products ->
-                Log.i(
-                    TAG,
-                    "PRODUCTS FETCHED : $products"
-                )
                 homeUiState = homeUiState.copy(
                     products = products.map { it.toUiProductObject() },
                     isLoading = false
@@ -79,7 +66,7 @@ class HomeViewModel(
     fun refreshProducts() {
         homeUiState = homeUiState.copy(isLoading = true)
         viewModelScope.launch {
-            getAllProducts()
+            checkAndUpdateDatabase()
         }
     }
 }
