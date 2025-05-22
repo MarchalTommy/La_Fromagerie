eca73fc2d68db03b0f3ce75550a55e9878f58af7
Marchal Tommy <marchal.tommy@gmail.com>
2025-05-22 16:21:17 +0200

    Version Bump to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.mtdevelopment.lafromagerie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.8.0"
+        versionCode = 12
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
