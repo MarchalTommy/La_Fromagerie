9d8d53e4e5a2a307e0e98dd7e23b083bec3dc8e5
Tommy <marchal.tommy@gmail.com>
2026-03-19 19:33:40 +0100

    Finished fixing AGP 9 for now... Temporary shit, but it will do the trick for now, enough for me to finish the base app !

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,45 @@ plugins {
     alias(libs.plugins.firebase.crashlytics.plugin) apply false
     alias(libs.plugins.ksp) apply false
 }
+
+// 1. On lit les versions ICI, en dehors du bloc subprojects
+val javaVersionStr = libs.versions.java.level.get()
+val javaVersionEnum = org.gradle.api.JavaVersion.toVersion(javaVersionStr)
+
+// 2. On applique la configuration
+subprojects {
+    // Configuration Kotlin
+    plugins.withId("org.jetbrains.kotlin.android") {
+        val kotlinExt =
+            extensions.getByName("kotlin") as org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+        kotlinExt.jvmToolchain(javaVersionStr.toInt())
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions.jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget(
+                    javaVersionStr
+                )
+            )
+        }
+    }
+
+    // Configuration Android (Module Application)
+    plugins.withId("com.android.application") {
+        val androidApp =
+            extensions.getByName("android") as com.android.build.api.dsl.ApplicationExtension
+        androidApp.compileOptions {
+            sourceCompatibility = javaVersionEnum
+            targetCompatibility = javaVersionEnum
+        }
+    }
+
+    // Configuration Android (Modules Library)
+    plugins.withId("com.android.library") {
+        val androidLib =
+            extensions.getByName("android") as com.android.build.api.dsl.LibraryExtension
+        androidLib.compileOptions {
+            sourceCompatibility = javaVersionEnum
+            targetCompatibility = javaVersionEnum
+        }
+    }
+}
