28c7c990a063e6edcfe3b0d8b4a23b36e922482e
Tommy <marchal.tommy@gmail.com>
2026-08-19 08:49:52 +0200

    Upgrade app version code for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.mtdevelopment.lafromagerie"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.0.0"
+        versionCode = 30
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
